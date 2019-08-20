4c7c1a73c323667448f416e5ba80d4497b91b211
Jairo  Campos <jairocampos@Jairos-MacBook-Pro.local>
2019-08-19 20:50:19 -0600

    added accordion module

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,52 @@
+/**
+ * Accordion functionality here
+ */
+const accordion = (function () {
+    let instance;
+   
+    function init() {
+   
+      function privateMethod(){
+          console.log( "I am private" );
+      }
+   
+      const privateVariable = "Im also private";
+   
+      const privateRandomNumber = Math.random();
+   
+      return {
+   
+        // Public methods and variables
+        publicMethod: function () {
+          console.log( "The public can see me!" );
+        },
+   
+        publicProperty: "I am also public",
+   
+        getRandomNumber: function() {
+          return privateRandomNumber;
+        }
+   
+      };
+   
+    };
+   
+    return {
+      getInstance: function () {
+   
+        if ( !instance ) {
+          instance = init();
+        }
+   
+        return instance;
+      }
+   
+    };
+   
+})();
+
 (function() {
    console.log('document ready');
- })("docReady", window);
+   // Create instance to add accordion to footer mobile
+   const footerAccordion = accordion.getInstance();
+})("docReady", window, accordion);
